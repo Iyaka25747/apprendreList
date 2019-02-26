0f82eda551caf3cb44879666e16641e42b2d6b66
Iyaka <ini@mordu.ch>
2019-02-26 08:52:37 +0100

    update

--- convertCSV2Json.py
+++ convertCSV2Json.py
@@ -5,21 +5,37 @@ from operator import itemgetter
 inData = []
 csvFile = open('Voc6_1.csv', 'rt')
 jsonFile = open('outfile_test.json', 'w')
-fieldnames = ("voc","page","line","type","der-die-das","motEtranger","motFR")
-reader = csv.reader(csvFile,fieldnames, delimiter = ';')
+reader = csv.DictReader(csvFile, delimiter = ';')
+
+# build dictionnary from file
+# voc;page;line;Type;Der-Die-Das;Mot en ALL;pluriel;Mot FR
 
 count = 0
+for row in reader:
+    
+
+    count += 1
+
+readertest = sorted(reader, key = itemgetter('type'))
+
+
+
 prevVoc =""
 prevPage =""
 for row in reader:
     print(row)
+    # group by - https://stackoverflow.com/questions/773/how-do-i-use-pythons-itertools-groupby 
+    # https://adiyatmubarak.wordpress.com/2015/10/05/group-list-of-dictionary-data-by-particular-key-in-python/
+    # students = sorted(students, key=itemgetter('class'))
+
     
     if count == 0: #skip the 1st line
         break
     while voc == prevVoc:
         while page == prevPage:
-            body = tout les lignes meme voc meme page
-    voc, page, line, motType, derDieDas, motEtranger, motFR = itemgetter(0,1,2,3,4,5,6)(row)
+        #     body = tout les lignes meme voc meme page
+                print("xx")
+#     voc, page, line, motType, derDieDas, motEtranger, motFR = itemgetter(0,1,2,3,4,5,6)(row)
 
     json.dump(row, jsonFile)
     jsonFile.write('\n')
--- test.py
+++ test.py
@@ -1,2 +1,9 @@
 ligne = {"type":"Vocabulaire","DeterminantFR":"Le", "DetDE":"Das","NomFR":"Pain","NomDE":"Brot"}
 print(ligne.get("DetDE"))
+
+import csv
+
+datafile = open('Voc6_1.csv', 'rt')
+myreader = csv.DictReader(datafile, delimiter = ';')
+for row in myreader:
+    print(row)
