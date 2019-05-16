8b07d7d8f10618916727ed26d543a0469bee79c6
Iyaka25747 <ini@mordu.ch>
2019-05-16 08:06:23 +0200

    Add files via upload

---
+++ convertCSV2Json.py
@@ -0,0 +1,51 @@
+import csv
+import json
+from operator import itemgetter 
+# #For YAML
+# import yaml
+# import io
+
+inData = []
+# csvFile = open('C:\\Users\\ini\\Documents\\Development_Source_code\\apprendreList\\Saisie-vocabulaire\\VocCSV.csv', 'rt', encoding="utf8")
+# jsonFile = open('C:\\Users\\ini\\Documents\\Development_Source_code\\apprendreList\\Voc-exercices.json', 'w', encoding="utf8")
+csvFile = open('C:\\Users\\ini\\OneDrive\\Documents OneDrive\\dev\\apprendreList\\Saisie-vocabulaire\\VocCSV.csv', 'rt', encoding="utf8")
+jsonFile = open('C:\\Users\\ini\\OneDrive\\Documents OneDrive\\dev\\apprendreList\\Voc-exercices.json', 'w', encoding="utf8")
+# csvFile = open('VocAll_toTransformCSV_debug.csv', 'rt', encoding="utf8")
+# jsonFile = open('exercices_vocAll_debug.json', 'w', encoding="utf8")
+
+reader = csv.DictReader(csvFile, delimiter = ';')
+
+# build dictionnary from file
+# voc;page;line;Type;Der-Die-Das;Mot en ALL;pluriel;Mot FR
+lessonsDict = dict()
+for row in reader:
+    # Check if this language already exists
+    language = row['language']
+    if  language not in lessonsDict:
+        lessonsDict[language] = {}
+    # Check if this voc already exists
+    voc = row['voc']
+    if  voc not in lessonsDict[language]:
+        lessonsDict[language][voc] = {}
+
+    page = str(row['page'])
+    if page not in lessonsDict[language][voc]:
+        lessonsDict[language][voc][page]={}
+    
+    line = row['line']
+    # if line not in lessonsDict[language][voc][page]:
+    #     lessonsDict[language][voc][page][line]=[]
+
+    del row['language']
+    del row['voc']
+    del row['page']
+    del row['line']
+    lessonsDict[language][voc][page][line] = row
+
+#Le fichier transformer est sauver dans un fichier pour archive
+json.dump(lessonsDict, jsonFile, indent=2, ensure_ascii=False )
+jsonFile.write('\n')
+
+# # Write YAML file
+# with io.open('data.yaml', 'w', encoding='utf8') as outfile:
+#     yaml.dump(lessonsDict, outfile, default_flow_style=False, allow_unicode=True)
